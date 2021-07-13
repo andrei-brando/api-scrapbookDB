3a06c57858cd3bcbb66214d03666eeb24e07bf66
andrei.brando <albertbrando@hotmail.com>
2021-07-12 22:00:27 -0300

    ajusta para conseguir subir no heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,25 @@
 require('dotenv').config();
 
+const rootPath = process.env.NODE_ENV?.toLocaleLowerCase() === 'production'
+  ? 'dist'
+  : 'src';
+
+
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
   entities: [
-    `${process.env.ENTITIES_DIR}/core/data/database/entities/**/*`,
+    `${rootPath}/core/data/database/entities/**/*`,
+  ],
+  migrations: [
+    `${rootPath}/core/data/database/migrations/**/*`,
   ],
   cli: {
-    entitiesDir: `${process.env.ENTITIES_DIR}/core/data/database/entities`,
-    migrationsDir: `${process.env.ENTITIES_DIR}/core/data/database/migrations`,
+    entitiesDir: `src/core/data/database/entities`,
+    migrationsDir: `src/core/data/database/migrations`,
   },
-  migrations: [
-    `${process.env.ENTITIES_DIR}/core/data/database/migrations/**/*`,
-  ],
   extra: {
     ssl: {
       rejectUnauthorized: false,
